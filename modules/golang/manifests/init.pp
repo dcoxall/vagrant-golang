4d8763772f811fbdaedeb7a00a4375c13d2daa33
Adrian Punga <adrian.punga@kalon.ro>
2014-12-24 15:00:40 +0200

    Added Mercurial too

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -26,9 +26,8 @@ class golang ( $version = "1.4" ) {
     unless  => "/bin/grep -q GOPATH /home/vagrant/.profile ; /usr/bin/test $? -eq 0"
   }
 
-  exec { "install-git":
-    command => "/usr/bin/apt-get -qy update &&  /usr/bin/apt-get -qy install git",
-    unless  => "/usr/bin/test -x /usr/bin/git"
+  exec { "install-git-mercurial":
+    command => "/usr/bin/apt-get -qy update &&  /usr/bin/apt-get -qy install git mercurial"
   }
 
 }
